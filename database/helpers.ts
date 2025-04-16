8a8b4dcdd107a0d4d09f2d7e7ffbbf54d031df55
Fergus Bentley <fergusbentley@gmail.com>
2025-04-16 19:49:10 +0100

    feat(database): :label: Validate dice notation at compile time

--- database/helpers.ts
+++ database/helpers.ts
@@ -12,15 +12,42 @@ export function query<Doc extends Document>(
 
 export function ref<Doc extends Document>(
   collectionId: CollectionID,
-  docId: string,
+  docId: Doc["id"],
 ): QueryFirst<Doc> {
   return new QueryFirst<Doc>(collectionId, [
     { field: "id", op: "==", value: docId },
   ]);
 }
 
-export function dice(diceString: string): string {
-  // TODO: validate dice notation
+// deno-fmt-ignore
+type Char =
+  | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m"
+  | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+  | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M"
+  | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
+
+type Ident<T extends string> = T extends `${Char}${infer Rest}`
+  ? T extends `${infer C}${Rest}` ? `${C}${Ident<Rest> | ""}`
+  : never
+  : never;
+
+type DropOrKeep = "d" | "D" | "k" | "K";
+type HighOrLow = "l" | "L" | "h" | "H";
+type DiceModifier = `${DropOrKeep}${HighOrLow}${number | ""}` | "";
+type DiceTerm<T extends string> =
+  | `${number | ""}d${number}${DiceModifier}`
+  | `${number}`
+  | Ident<T>;
+type DiceOperator = "+" | "-" | "*" | "/";
+type DiceString<T extends string> = T extends DiceTerm<T> ? T
+  : T extends `${infer F} ${DiceOperator} ${infer R}`
+    ? T extends `${F} ${infer O} ${R}` ? `${DiceTerm<F>} ${O} ${DiceString<R>}`
+    : never
+  : never;
+
+export function dice<T extends string>(
+  diceString: T extends DiceString<T> ? T : DiceString<T>,
+): T {
   return diceString;
 }
 
@@ -28,14 +55,17 @@ type CostString = `${number} ${Currency}`;
 export function cost(costString: CostString): Cost {
   const [amount, currency] = costString.split(" ");
   return {
-    amount: parseInt(amount, 10),
+    amount: parseFloat(amount),
     currency,
   } as Cost;
 }
 
-export function dmg(diceString: string, type: string): Resolvable<Damage> {
+export function dmg<T extends string>(
+  diceString: DiceString<T>,
+  type: string,
+): Resolvable<Damage> {
   return {
-    dice: dice(diceString),
+    dice: diceString,
     type: ref("damageTypes", type),
   };
 }
