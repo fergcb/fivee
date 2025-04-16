17b0e4e0570ad0aabcb578c74da67a8b9027b747
Fergus Bentley <fergusbentley@gmail.com>
2025-04-17 00:30:16 +0100

    ci(meta): :construction_worker: Start using Yarn to build docs

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
+import docusaurusPreset from "@docusaurus/core/lib/babel/preset.js"
+
+export default {
+  presets: [docusaurusPreset],
 };
